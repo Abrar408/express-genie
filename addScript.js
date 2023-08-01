d2031790e265914399130c6a8120af3e1c49ca8c
Abrar-internative <abrar.internativelabs@gmail.com>
2023-08-01 17:47:57 +0500

    minor bugs fixed

--- addScript.js
+++ addScript.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const userPackageJsonPath = path.join(__dirname, "./../../package.json");
+const userPackageJsonPath = path.join(__dirname, "../../package.json");
 const userPackageJson = require(userPackageJsonPath);
 
 // Modify the user's package.json here
@@ -8,7 +8,7 @@ if (!userPackageJson.scripts) {
   userPackageJson.scripts = {};
 }
 
-userPackageJson.scripts["setup"] = "node ./node_modules/express-setup/index.js";
+userPackageJson.scripts["setup"] = "node ./node_modules/express-genie/index.js";
 
 // Write the modified package.json back to the user's file system
 fs.writeFileSync(userPackageJsonPath, JSON.stringify(userPackageJson, null, 2));
